dc2b5de8f5f1c7549b1fee0411fd49d87e5a7bbd
NikkiBeeLili <arcanahereticae13@gmail.com>
2021-05-26 22:41:04 -0500

    sort started

--- src/app/components/home-directory/home/home.component.ts
+++ src/app/components/home-directory/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs'; 
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string;
 
   constructor() { }
+  ngOnDestroy(): void {
+    
+  }
 
   ngOnInit() {
   }
